b1eec9a85f7724641722b1314f38a70531674dd9
Ritik Singh <rs1356432@gmail.com>
2026-02-12 22:04:49 +0530

    Add upload endpoint and CORS config

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,11 +2,10 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from app.database.database import Base,engine
 from app.routing.endpoints import router
+from app.routing.resume import router1
 from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
-
-
 
 Base.metadata.create_all(bind = engine)
 app = FastAPI()
@@ -16,8 +15,10 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
     ],
-    allow_credentials=True,   # 🔴 REQUIRED for cookies
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -27,3 +28,4 @@ def health():
     return {"message":"fastapi working"}
 
 app.include_router(router)
+app.include_router(router1)
